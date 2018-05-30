3bbdee258074fcc46360e42011398652a9b6ea98
Maicon E. Prange <maicon.prange@amcom.com.br>
2018-05-30 17:16:30 -0300

    Atualização

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
-const webpack = require('webpack');
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+const extractCSS = new ExtractTextPlugin({ filename: 'bundle.css' })
 
 module.exports = {
     entry: './ex/index.js',
@@ -19,6 +22,15 @@ module.exports = {
                 presets: ['es2015', 'react'],
                 plugins: ['transform-object-rest-spread']
             }
-        }]
-    }
+        }, {
+            test: /\.css$/,
+            use: extractCSS.extract({
+                fallback: 'style-loader',
+                use: ['css-loader']
+            })
+        },]
+    },
+    plugins: [
+        extractCSS
+    ],
 }
